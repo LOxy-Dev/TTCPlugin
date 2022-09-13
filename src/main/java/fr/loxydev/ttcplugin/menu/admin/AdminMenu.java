1f55faa3e7f9ad8e313dff2ae23acb56a8046888
Hector Fiorese <loxygaming78@gmail.com>
2022-09-13 16:41:16 +0200

    Add close item in admin menu

--- src/main/java/fr/loxydev/ttcplugin/menu/admin/AdminMenu.java
+++ src/main/java/fr/loxydev/ttcplugin/menu/admin/AdminMenu.java
@@ -1,10 +1,13 @@
 package fr.loxydev.ttcplugin.menu.admin;
 
+import fr.loxydev.ttcplugin.database.ItemDataHandler;
 import fr.loxydev.ttcplugin.menu.Menu;
 import fr.loxydev.ttcplugin.menu.PlayerMenuUtility;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class AdminMenu extends Menu {
     @Override
@@ -19,12 +22,19 @@ public class AdminMenu extends Menu {
 
     @Override
     public void handleMenu(InventoryClickEvent e, PlayerMenuUtility playerMenuUtility) {
+        if (e.getCurrentItem() == FILLER_GLASS || e.getCurrentItem() == null) return;
 
+        Player player = playerMenuUtility.getPlayer();
+
+        if (e.getCurrentItem().getType() == Material.BARRIER) player.closeInventory();
     }
 
     @Override
     public void setMenuItems(PlayerMenuUtility playerMenuUtility) {
         ItemStack close = new ItemStack(Material.BARRIER);
+        ItemMeta closeMeta = close.getItemMeta();
+        closeMeta.setDisplayName("Close menu");
+        close.setItemMeta(closeMeta);
 
         inventory.setItem(22, close);
 
