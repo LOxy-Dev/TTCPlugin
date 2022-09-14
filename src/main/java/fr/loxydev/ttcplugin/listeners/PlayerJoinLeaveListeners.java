38e0c00821ca0b0443bd6f6fb619ef45760f02be
Hector Fiorese <loxygaming78@gmail.com>
2022-09-14 18:08:21 +0200

    Custom Login message

--- src/main/java/fr/loxydev/ttcplugin/listeners/PlayerJoinLeaveListeners.java
+++ src/main/java/fr/loxydev/ttcplugin/listeners/PlayerJoinLeaveListeners.java
@@ -1,6 +1,7 @@
 package fr.loxydev.ttcplugin.listeners;
 
 import fr.loxydev.ttcplugin.TheTerrierCityPlugin;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,13 +14,17 @@ public class PlayerJoinLeaveListeners implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        // Every time a player joins, assign them a personal LockManagerMenu object
+        event.setJoinMessage("[" + ChatColor.GREEN + "+" + ChatColor.RESET + "] " + ChatColor.ITALIC + ChatColor.BOLD + player.getName());
+
+        // Every time a player joins, assign them a personal MenuUtility object
         TheTerrierCityPlugin.getPlayerMenuUtility(player);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
+
+        event.setQuitMessage("[" + ChatColor.DARK_RED + "-" + ChatColor.RESET + "] " + ChatColor.ITALIC + ChatColor.BOLD + player.getName());
 
         // Since the player left, get rid of the ShopManagerMenu objects for that player
         if (TheTerrierCityPlugin.playerList.containsKey(player))
