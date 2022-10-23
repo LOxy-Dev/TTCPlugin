f6abca656fe6f50877ae62d268fa73b1da0988b9
Hector Fiorese <loxygaming78@gmail.com>
2022-10-23 15:07:16 +0200

    Holograms

--- src/main/java/fr/loxydev/ttcplugin/commands/subcommands/TestCommand.java
+++ src/main/java/fr/loxydev/ttcplugin/commands/subcommands/TestCommand.java
@@ -1,11 +1,16 @@
 package fr.loxydev.ttcplugin.commands.subcommands;
 
+import fr.loxydev.ttcplugin.TheTerrierCityPlugin;
 import fr.loxydev.ttcplugin.commands.SubCommand;
+import fr.loxydev.ttcplugin.utils.Hologram;
 import io.github.bananapuncher714.nbteditor.NBTEditor;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
+
+import java.util.ArrayList;
 
 public class TestCommand extends SubCommand {
     @Override
@@ -26,13 +31,39 @@ public class TestCommand extends SubCommand {
     @Override
     public void perform(Player player, String[] args) {
         if (player.hasPermission("ttc.admin")) {
-            if(player.hasPermission("test.setvillager")) {
-                Villager v = (Villager) player.getWorld().spawnEntity(player.getLocation(), EntityType.VILLAGER);
-                v.setCustomName("Test Shop");
-                v.setSilent(true);
-                v.setProfession(Villager.Profession.NITWIT);
-                NBTEditor.set(v, true, "NoAI");
-            }
+            /*// Test shop summon
+            Villager v = (Villager) player.getWorld().spawnEntity(player.getLocation(), EntityType.VILLAGER);
+            v.setCustomName("Test Shop");
+            v.setSilent(true);
+            v.setProfession(Villager.Profession.NITWIT);
+            NBTEditor.set(v, true, "NoAI");
+            */
+
+            // Test hologram summon
+            ArrayList<String> message = new ArrayList<>();
+            message.add("This is a test hologram...");
+            message.add("Here is a §4RED COLOR.");
+            message.add("Are the lines correctly spaced???");
+            message.add("I hope it works.");
+            Hologram test = new Hologram(player.getLocation(), message);
+
+            Bukkit.getScheduler().runTaskLater(TheTerrierCityPlugin.getPlugin(), new Runnable() {
+                @Override
+                public void run() {
+                    ArrayList<String> new_msg = new ArrayList<>();
+                    new_msg.add("I just became updated!!!");
+
+                    test.updateText(new_msg);
+                }
+            }, 20*5);
+
+            Bukkit.getScheduler().runTaskLater(TheTerrierCityPlugin.getPlugin(), new Runnable() {
+                @Override
+                public void run() {
+                    test.remove();
+                }
+            }, 20*10);
+
         } else {
             player.sendMessage(ChatColor.RED + "This command is for admins.");
         }
---
+++ src/main/java/fr/loxydev/ttcplugin/utils/Hologram.java
@@ -0,0 +1,59 @@
+package fr.loxydev.ttcplugin.utils;
+
+import fr.loxydev.ttcplugin.TheTerrierCityPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+
+import java.util.ArrayList;
+
+public class Hologram {
+
+    private final ArrayList<ArmorStand> armorStands;
+    private final Location loc;
+
+    public Hologram(Location loc, ArrayList<String> text) {
+        this.loc = loc;
+        this.armorStands = new ArrayList<>();
+
+        int level = 0;
+        for (String line : text) {
+            Location asLoc = new Location(loc.getWorld(), loc.getX(), loc.getY() - level*0.25, loc.getZ());
+            ArmorStand armorStand = (ArmorStand) TheTerrierCityPlugin.lobby.spawnEntity(asLoc, EntityType.ARMOR_STAND);
+
+            armorStand.setCustomName(ChatColor.translateAlternateColorCodes('§', line));
+
+            armorStand.setGravity(false);
+            armorStand.setVisible(false);
+            armorStand.setCustomNameVisible(true);
+
+            armorStands.add(armorStand);
+
+            level++;
+        }
+    }
+
+    public void updateText(ArrayList<String> text) {
+        remove();
+
+        for (int i = 0; i < text.size(); i++) {
+            Location asLoc = new Location(loc.getWorld(), loc.getX(), loc.getY() - i*0.25, loc.getZ());
+            ArmorStand armorStand = (ArmorStand) TheTerrierCityPlugin.lobby.spawnEntity(asLoc, EntityType.ARMOR_STAND);
+
+            armorStand.setCustomName(ChatColor.translateAlternateColorCodes('§', text.get(i)));
+
+            armorStand.setGravity(false);
+            armorStand.setVisible(false);
+            armorStand.setCustomNameVisible(true);
+
+            armorStands.add(armorStand);
+        }
+    }
+
+    public void remove() {
+        for (ArmorStand armorStand : armorStands) {
+            armorStand.remove();
+        }
+    }
+}
